bd3dd9337af963dd1377d707ec53151ebefb6d05
jorgealexis07 <201711104@tese.edu.mx>
2022-04-22 14:00:26 -0500

    paso 7. Agregue el metodo get info para su testeo

--- app/spiderman.js
+++ app/spiderman.js
@@ -7,6 +7,12 @@ class Spiderman{
         this.movies= movies;
         this.studio=studio;
     }
-}
 
+    getInfo() {
+        return `Hey, I'm ${this.actor} from ${this.studio} studio`;
+      }
+
+}
+const tomHolland = new Spiderman("Spiderman Marvel",25,"Tom Holland",5, "Marvel")
+console.log(tomHolland.getInfo());
 module.exports = Spiderman
